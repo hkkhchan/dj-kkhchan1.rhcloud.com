3e42f21de78caf8ac64f3f359f234ca6c06314a3
Win10 <mrkhchan@outlook.com>
2016-08-04 23:09:35 +0800

    views

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from myproject import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^h/', views.hello, name='hello'),
