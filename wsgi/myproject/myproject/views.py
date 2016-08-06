4b139a69eb2b9e4511ca79a6877757c2816d1a94
Win10 <mrkhchan@outlook.com>
2016-08-06 12:01:28 +0800

    debug test

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-import pdb
 
 def hello(request):
-    pdb.set_trace()
+    import pdb;pdb.set_trace()
     return HttpResponse("Hello world ! It is my/my/views.py")
