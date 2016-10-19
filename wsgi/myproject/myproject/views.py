37a85e0502a9573513678df1d46ed714781651ab
hkkhchan <hkkhchan@gmail.com>
2016-10-20 00:21:05 +0800

    template

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -1,5 +1,9 @@
+from django.template import Template, Context
 from django.http import HttpResponse
 import pdb
 
 def hello(request):
     return HttpResponse("Hello world ! It is my/my/views.py")
+
+def intro(request):
+    
