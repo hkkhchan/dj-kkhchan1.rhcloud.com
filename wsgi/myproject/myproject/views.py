99c66bf76525db910bf8f92d209913756a2df4c5
hkkhchan <hkkhchan@gmail.com>
2016-10-20 23:59:37 +0800

    add welcome

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -2,9 +2,16 @@ from django.template.loader import get_template
 from django.template import Template, Context
 from django.http import HttpResponse
 import pdb
+import datetime
 
 def hello(request):
     return HttpResponse("Hello world ! It is my/my/views.py")
 
 def intro(request):
     return HttpResponse("Under developement")
+
+def welcome(request):
+    now = datetime.datetime.now()
+    t = get_template('welcome.html')
+    html = t.render(Context({'current_date':now}))
+    return HttpResponse(html)
