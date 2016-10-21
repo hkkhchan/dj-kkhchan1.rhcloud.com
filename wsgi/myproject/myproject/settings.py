4b0b5b9c6ce25314eebacf6437acb4a603604094
hkkhchan <hkkhchan@gmail.com>
2016-10-21 13:16:50 +0800

    change static location

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -94,6 +94,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/media/'
+# STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
