5172c5933e16a87fe59cd091c806ed176c08643d
hkkhchan <hkkhchan@gmail.com>
2016-10-21 23:27:06 +0800

    start-template

--- wsgi/static/js/start.js
+++ wsgi/static/js/start.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
+  var page=0;
   $(document).keydown(function(e){
-    if ($('.start-wrapper').is(':visible')){
+    if (page===0){
       $('.start-wrapper').hide();
       $('.option-wrapper').show();
+      page=1;
+    }
+    else if (page===1){
+      switch(e.keyCode){
+        case 38://up
+          if ($('.play').hasClass('selected')){
+            $('.play, .setting').toggleClass('selected');
+          }
+          else{
+            $('.selected').removeClass('selected').prev().addClass('selected');
+          }
+          break;
+        case 40:
+          if ($('.setting').hasClass('selected')){
+            $('.play, .setting').toggleClass('selected');
+          }
+          else{
+            $('.selected').removeClass('selected').next().addClass('selected');
+          }
+          break;
+        default:
+          break;
+      }
     }
   });
 });
