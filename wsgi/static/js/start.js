6627c2849b4a3d05e0b56566fad8767f74461a9d
hkkhchan <hkkhchan@gmail.com>
2016-10-22 13:11:19 +0800

    menu

--- wsgi/static/js/start.js
+++ wsgi/static/js/start.js
@@ -7,26 +7,35 @@ $(document).ready(function(){
       page=1;
     }
     else if (page===1){
-      switch(e.keyCode){
-        case 38://up
-          if ($('.play').hasClass('selected')){
-            $('.play, .setting').toggleClass('selected');
-          }
-          else{
-            $('.selected').removeClass('selected').prev().addClass('selected');
-          }
-          break;
-        case 40:
-          if ($('.setting').hasClass('selected')){
-            $('.play, .setting').toggleClass('selected');
-          }
-          else{
-            $('.selected').removeClass('selected').next().addClass('selected');
-          }
-          break;
-        default:
-          break;
-      }
+      option_nav(e.keyCode);
     }
   });
+  
+  var option_nav=function (keyCode){
+    switch(e.keyCode){
+      case 38: //up arrow
+        if ($('.play').hasClass('selected')){
+          $('.play, .setting').toggleClass('selected');
+        }
+        else{
+          $('.selected').removeClass('selected').prev().addClass('selected');
+        }
+        break;
+      case 40: //down arrow
+        if ($('.setting').hasClass('selected')){
+          $('.play, .setting').toggleClass('selected');
+        }
+        else{
+          $('.selected').removeClass('selected').next().addClass('selected');
+        }
+        break;
+      case 27: //Esc
+        page=0;
+        $('.option-wrapper').hide();
+        $('.start-wrapper').show();
+        break;
+      default:
+        break;
+    }
+  }
 });
