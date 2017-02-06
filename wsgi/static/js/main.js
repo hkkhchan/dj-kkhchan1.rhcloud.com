8bf49c248a4c65f1f401641e635f3dccb8da2b44
kenneth <wkkhchan@gmail.com>
2017-02-07 01:24:09 +0800

    minor

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -1,5 +1,3 @@
-var before_data = {before_ptr : 0};
-var after_data = {after_ptr : 0};
 Vue.component("before_login",{
   template : '<div> \
     <div class="topic-wrapper"> \
@@ -15,11 +13,30 @@ Vue.component("before_login",{
       <div :class="before_ptr == 4?\'selected\':\'\'">Setting</div> \
     </div> \
   </div>',
-  props : {
-    before_ptr: String
-  },
   data : function(){
-    return before_data;
+    return {
+      before_ptr: 0
+    };
+  },
+  created: function(){
+    window.addEventListener('keydown',this.key)
+  },
+  methods: {
+    key : function(e){
+      if (this.before_ptr > 0){
+        if (this.before_ptr > 1 && e.key == 'ArrowUp'){
+          this.before_ptr--;
+        } else if (this.before_ptr < 4 && e.key == 'ArrowDown'){
+          this.before_ptr++;
+        } else if (e.key == 'Escape'){
+          this.before_ptr = 0;
+        } else if (e.key == 'Enter'){
+          console.log('run ' + this.before_ptr);
+        }
+      } else {
+        this.before_ptr = 1;
+      }
+    }
   }
 });
 
@@ -51,43 +68,4 @@ var vm = new Vue({
   data : {
     currentView : "before_login"
   },
-  created: function(){
-    window.addEventListener('keydown',this.key)
-  },
-  methods: {
-    key : function (e){
-      switch(this.currentView){
-        case 'before_login':
-          if (this.before_ptr > 0){
-            if (this.before_ptr > 1 && e.key == 'ArrowUp'){
-              this.before_ptr--;
-            } else if (this.before_ptr < 4 && e.key == 'ArrowDown'){
-              this.before_ptr++;
-            } else if (e.key == 'Escape'){
-              this.before_ptr = 0;
-            } else if (e.key == 'Enter'){
-              console.log('run ' + this.before_ptr);
-            }
-          } else {
-            this.before_ptr = 1;
-          }
-          break;
-        case 'after_login':
-          if (this.after_ptr > 0){
-            if (this.after_ptr > 1 && e.key == 'ArrowUp'){
-              this.after_ptr--;
-            } else if (this.after_ptr < 3 && e.key == 'ArrowDown'){
-              this.after_ptr++;
-            } else if (e.key == 'Escape'){
-              this.after_ptr = 0;
-            } else if (e.key == 'Enter'){
-              console.log('run ' + this.after_ptr);
-            }
-          } else {
-            this.after_ptr = 1;
-          }
-          break;
-      }
-    }
-  }
 });
