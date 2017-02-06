2f044702c89504acb99473e465859510dbeda17d
kenneth <wkkhchan@gmail.com>
2017-02-07 00:48:24 +0800

    minor

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -50,7 +50,7 @@ var vm = new Vue({
     after_ptr : 0,
     currentView : "before_login"
   },
-  template : '<div v-component="{{currentView}}">',
+  template : '<div v-component="{{currentView}}"></div>',
   created: function(){
     window.addEventListener('keydown',this.key)
   },
